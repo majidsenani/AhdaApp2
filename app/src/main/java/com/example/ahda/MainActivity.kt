6d093dd13cacce69bb37cdddee713413b511585a
majidsenani <almnarr@gmail.com>
2026-08-21 23:40:23 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/ahda/MainActivity.kt
+++ app/src/main/java/com/example/ahda/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.dp
 import java.util.Locale
 
